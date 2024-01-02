c4a90a5bd9d060e55d91ccc6a6212f2e8375d16f
Gamahusen9 <gamahusein2019@gmail.com>
2024-01-02 08:25:10 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing_text", {
+    strings: ["Backend Developer", "Fullstack", "iuewhfehw"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+});
